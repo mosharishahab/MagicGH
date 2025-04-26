8ecd29d0892a40fd3e1ce69188d9f2fc6809b9f4
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-26 05:34:05 +0330

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>سلام شهاب جون!</h1>
+      <p>پروژه React و Vite با موفقیت اومده بالا.</p>
+    </div>
+  )
+}
