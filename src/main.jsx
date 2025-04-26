99146d8d30e698a41fdee27e96b0e02f80740ec1
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-26 04:59:31 +0330

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Story from './pages/Story'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/story" element={<Story />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
