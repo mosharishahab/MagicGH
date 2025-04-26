68af492d744c04fd9243659929e344b505c9ddde
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-26 05:35:25 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Story from './pages/Story'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/story" element={<Story />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
