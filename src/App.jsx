5f50dec3491fd313d38385820eb074e6a1b9e44f
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-26 05:53:39 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="text-center mt-32 font-sans">
+      <h1 className="text-3xl font-bold text-blue-600">سلام شهاب جون!</h1>
+      <p className="mt-4 text-gray-600">پروژه با Tailwind روی GitHub Pages بالا اومد!</p>
+    </div>
+  )
+}
